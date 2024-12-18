e10d36522279881a123e750882a6b485faa324c7
RyanPalomo <139880984+RyanPalomo@users.noreply.github.com>
2024-12-19 01:41:22 +0800

    image and color try

--- A05/personalities.php
+++ A05/personalities.php
@@ -14,17 +14,20 @@ class Personality{
     $this->image = $image;
   }
 
-  public function generateCard(){
+  public function generateCard() {
     return '
-    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-xs-12 card py-5">
-      <div class="card p-4 rounded-5">
-        <div class="h3">
+    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-xs-12 py-5">
+      <div class="card p-4 rounded-5" style="background-color: '.$this->color.';">
+        <div class="p-2">
+          <img src="'.$this->image.'" class="card-img-top" alt="'.$this->name.'">
+        </div> 
+        <div class="h3 text-center">
           '.$this->name.'
         </div>
-        <div class="mb-3 text-secondary">
+        <div class="mb-3 text-secondary text-center">
           '.$this->shortDescription.'
         </div>
-        <div class="mb-3">
+        <div class="mb-3 text-center">
           '.$this->longDescription.'
         </div>
       </div>
