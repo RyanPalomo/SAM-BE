2b14890f41c9de7c14bcd0ddcc8df5c4fcfcc898
RyanPalomo <palomoryan20@gmail.com>
2024-12-20 12:17:47 +0800

    fixing contents

--- A05/personalities.php
+++ A05/personalities.php
@@ -1,15 +1,16 @@
 <?php
-class Personality{
+class Personality
+{
 
-  public $id;  
+  public $id;
   public $name;
   public $shortDescription;
   public $longDescription;
   public $color;
   public $image;
-  public $status;
 
-  public function __construct($id, $name, $shortDescription, $longDescription, $color, $image){
+  public function __construct($id, $name, $shortDescription, $longDescription, $color, $image)
+  {
     $this->id = $id;
     $this->name = $name;
     $this->shortDescription = $shortDescription;
@@ -18,30 +19,77 @@ class Personality{
     $this->image = $image;
   }
 
-  public function generateCard() {
+  public function generateCard()
+  {
 
     $link = "personality-template.php?id=" . $this->id;
-  
+
     return '
     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 py-5 border-bottom">
       <a href="' . $link . '" class="card-link" style="text-decoration: none;">
-        <div class="card p-4 rounded-42" style="background-color: '.$this->color.'; height: 100%;">
+        <div class="card p-4 rounded-42" style="background-color: ' . $this->color . '; height: 100%;">
           <div class="p-2">
-            <img src="'.$this->image.'" class="card-img-top rounded" alt="'.$this->name.'">
+            <img src="' . $this->image . '" class="card-img-top rounded" alt="' . $this->name . '">
           </div> 
           <div class="h1 text-center pt-2">
-            '.$this->name.'
+            ' . $this->name . '
           </div>
           <div class="mb-3 text-center" style="color:white;">
-            '.$this->shortDescription.'
+            ' . $this->shortDescription . '
           </div>
           <div class="mb-3 text-center">
-            <a href="' . $link . '" class="btn btn-primary">Read More</a>
+            <a href="' . $link . '" class="btn btn-primary" style="background-color: '. $this->color.'; border-color: white;">Read More</a>
           </div>
         </div>
       </a>
     </div>
     ';
   }
+}
+
+
+
+class IslandContent
+{
+
+  public $id;
+
+  public $pkid;
+
+  public $image;
+
+  public $content;
+
+  public $color;
+
+  public function __construct($id, $pkid, $image, $content, $color)
+  {
+
+    $this->id = $id;
+
+    $this->pkid = $pkid;
+
+    $this->image = $image;
+
+    $this->content = $content;
+
+    $this->color = $color;
+
+  }
+
+  public function generateContent()
+{
+    return '
+    <div class="col-md-4 mb-4">
+      <div class="card shadow border-0"> 
+        <img src="' . $this->image . '" alt="' . $this->content . '" class="card-img-top img-fluid rounded">
+        <div class="card-body">
+            <p class="text-center"  style="color:' . $this->color . ';">
+              ' . $this->content . '
+            </p>
+        </div>
+      </div>
+    </div>';
+}
 }
 ?>
--- A05/personality-template.php
+++ A05/personality-template.php
@@ -3,6 +3,8 @@
 include('connect.php');
 include('personalities.php');
 
+$islandContents = array();
+
 // Get the `id` from the URL and fetch the personality
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
@@ -15,41 +17,84 @@ if (isset($_GET['id'])) {
         echo "Personality not found.";
         exit;
     }
-} else {
-    echo "No personality selected.";
-    exit;
+
+    // Fetch contents of the `islandcontents` table
+    $contentQuery = "SELECT * FROM islandcontents WHERE islandOfPersonalityID = $id";
+    $contentResult = mysqli_query($conn, $contentQuery);
+
+    while ($contentRow = mysqli_fetch_assoc($contentResult)) {
+        $a = new IslandContent($contentRow['islandContentID'], $contentRow['islandOfPersonalityID'], $contentRow['image'], $contentRow['content'], $contentRow['color']);
+
+        array_push($islandContents, $a);
+    }
+
 }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo htmlspecialchars($personality['name']); ?> - Personality Details</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title><?php echo htmlspecialchars($personality['name']); ?> Island</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
 
-    <div class="personality-details">
-        <h1 class="text-center py-5" style="color: <?php echo htmlspecialchars($personality['color']); ?>;"><?php echo htmlspecialchars($personality['name']); ?></h1>
+    <div class="personality-details container">
+        <h1 class="text-center py-5" style="color: <?php echo htmlspecialchars($personality['color']); ?>;">
+            <?php echo htmlspecialchars($personality['name']); ?>
+        </h1>
         <div class="container d-flex justify-content-center">
             <div class="row">
                 <div class="col-12">
-            <img src="<?php echo htmlspecialchars($personality['image']); ?>" alt="<?php echo htmlspecialchars($personality['name']); ?>" class="img-fluid rounded shadow mb-5">
-            </div>
+                    <img src="<?php echo htmlspecialchars($personality['image']); ?>"
+                        alt="<?php echo htmlspecialchars($personality['name']); ?>"
+                        class="img-fluid rounded shadow mb-5">
+                </div>
             </div>
         </div>
-        <div class="container text-center">
-            <p style="color: <?php echo htmlspecialchars($personality['color']); ?>;"><?php echo nl2br(htmlspecialchars($personality['longDescription'])); ?></p>
+        <div class="container text-center border-bottom">
+            <div class="row">
+                <div class="col-12">
+                    <p class="mx-auto"
+                        style="color: <?php echo htmlspecialchars($personality['color']); ?>; width:80%;">
+                        <?php echo nl2br(htmlspecialchars($personality['longDescription'])); ?>
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="container text-center mt-4">
+
+        <!-- Island Contents Section -->
+        <div class="container mt-5">
+            <h2 class="text-center py-3" style="color: <?php echo htmlspecialchars($personality['color']); ?>;">Island Contents</h2>
+            <?php if (!empty($islandContents)): ?>
+                <div class="row" id="islandcontent">
+                    <?php
+                    foreach ($islandContents as $islandcontent) {
+                        echo $islandcontent->generateContent();
+                    }
+                    ?>
+                </div>
+            <?php else: ?>
+                <p class="text-center">No contents available for this island.</p>
+            <?php endif; ?>
+        </div>
+
+
+
+        <div class="container text-center my-4">
             <a href="index.php" class="btn btn-primary">Back to Personalities</a>
         </div>
     </div>
 
-    
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
